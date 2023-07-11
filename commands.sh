d047612053d48cac7b9203617569dba3d43db7b9
rxzheng <richardxzheng@outlook.com>
2023-07-11 21:05:53 +1000

    Created commands and README

---
+++ commands.sh
@@ -0,0 +1,15 @@
+#do not execute any command for anything that you would not like
+#this just gives any idea for things you might want to install, and what can be installed
+apk add ranger
+apk add tmux
+apk add git
+apk add youtube-dl
+apk add speedtest-cli
+apk add ncurses
+apk add automake
+apk add autoconf
+apk add gcc
+apk add neofetch
+vim /etc/motd
+#write whatever you would like to write: this will greet you every time you open ish
+
